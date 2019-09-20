1ee699e4242943caaacec19bacd19465c9d7fa90
Scott Young <youngscottj@gmail.com>
2019-09-19 18:45:15 -0600

    add filter and click to mark complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import List from './components/todos/List';
 import TodoForm from './components/todos/TodoForm';
+import Footer from './components/todos/Footer';
 
 class App extends Component {
   state = {
@@ -8,20 +9,28 @@ class App extends Component {
       { id: 1, name: 'Learn Rails', complete: true },
       { id: 2, name: 'Learn React', complete: false },
       { id: 3, name: 'Learn React Router', complete: false }
-    ]
+    ],
+    filter: 'All'
+  }
+
+  setFilter = (filter) => {
+                    // filter: filter
+    this.setState({ filter })
   }
 
   getUniqId = () => {
-   //NOTE We are just using this as a helper function for id's since we aren't using a db yet
-   return Math.floor((1 + Math.random()) * 0x10000)
-     .toString(16)
-     .substring(1);
-  }
+    //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+   }
+
 
   addTodo = (name) => {
     const { todos } = this.state
+                    // name: name
     const todo = { name, complete: false, id: this.getUniqId() }
-    this.setState({ todos: [todo, ...todos]})
+    this.setState({ todos: [todo, ...todos] })
   }
   // renderTodos = () => {
   //   const { todos } = this.state
@@ -32,15 +41,43 @@ class App extends Component {
   //   })
   // }
 
+  handleClick = (id) => {
+    const { todos } = this.state
+    this.setState({
+      todos: todos.map( todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete
+          }
+        }
+        return todo
+      })
+    })
+  }
+
+  visibleItems = () => {
+    const { todos, filter } = this.state
+    switch(filter) {
+      case 'Active':
+        return todos.filter( t => !t.complete )
+      case 'Completed':
+        return todos.filter( t => t.complete )
+      default:
+        return todos
+    }
+  }
+
   render() {
-     const { todos } = this.state
-     return (
-       <div>
-         <TodoForm addTodo={this.addTodo} />
-         <List name="Bucket List" items={todos} />
-       </div>
-     )
-   }
- }
+    const { todos, filter } = this.state
+    return (
+      <div>
+        <TodoForm addTodo={this.addTodo} />
+        <List name="Bucket List" items={this.visibleItems()} todoClick={this.handleClick} />
+        <Footer filter={filter} setFilter={this.setFilter} />
+      </div>
+    )
+  }
+}
 
 export default App;
